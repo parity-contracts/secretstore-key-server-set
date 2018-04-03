5e9b1ba9cb2eb0e7978fbe681eca3dce7e75063b
Svyatoslav Nikolsky <svyatonik@gmail.com>
2018-04-03 15:29:29 +0300

    old tests

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/parity-contracts/secretstore-key-server-set.svg?branch=master)](https://travis-ci.org/parity-contracts/secretstore-key-server-set)
+[![Coverage Status](https://coveralls.io/repos/github/parity-contracts/secretstore-key-server-set/badge.svg)](https://coveralls.io/github/parity-contracts/secretstore-key-server-set)
+
+# secretstore-key-server-set
+Secret Store key servers set contract with migration support.
---
+++ test/helpers/crypto.js
@@ -0,0 +1,52 @@
+const EthJsUtil = require("ethereumjs-util");
+
+import {secp256k1, toBuffer, bufferToHex} from "ethereumjs-util";
+
+export function parseSignature(signatureString) {
+  return {
+    r: signatureString.slice(0, 66),
+    s: '0x' + signatureString.slice(66, 130),
+    v: web3.toHex(27 + Number.parseInt(signatureString.slice(130), 16))
+  }
+};
+
+// Since https://github.com/ethereum/go-ethereum/commit/b59c8399fbe42390a3d41e945d03b1f21c1a9b8d
+// `eth_sign` messes with message to sign before actually signing; since we're only signing from the code,
+// we want to avoid that prefixing; so this function mimicks the actual signing used in production.
+export function ethSignWithoutPrefixing(privateKey, messageHashInHex) {
+  let res = secp256k1.sign(toBuffer(messageHashInHex), toBuffer(privateKey));
+  return {
+    r: bufferToHex(res.signature.slice(0, 32)),
+    s: bufferToHex(res.signature.slice(32, 64)),
+    v: res.recovery + 27
+  }
+}
+
+export function multisignWithoutPrefixing(message, privateKeys) {
+  let vs = [];
+  let rs = [];
+  let ss = [];
+
+  privateKeys.forEach(privateKey => {
+    let signature = ethSignWithoutPrefixing(privateKey, message);
+    vs.push(signature.v);
+    rs.push(signature.r);
+    ss.push(signature.s);
+  });
+
+  return [ vs, rs, ss ];
+}
+
+export function recoverPublic(account) {
+  // sign message
+  const msg = new Buffer('hello');
+  const sig = web3.eth.sign(account, '0x' + msg.toString('hex'));
+  const res = EthJsUtil.fromRpcSig(sig);
+
+  // ...and recover public from the signature
+  const prefix = new Buffer("\x19Ethereum Signed Message:\n");
+  const prefixedMsg = EthJsUtil.sha3(Buffer.concat([prefix, new Buffer(String(msg.length)), msg]));
+  const pub = EthJsUtil.bufferToHex(EthJsUtil.ecrecover(prefixedMsg, res.v, res.r, res.s));
+
+  return pub;
+}
